1822877b1084491079cf9e250ed17272796e263e
wjdgkals23 <wjdgkals74@gmail.com>
2019-10-31 17:51:42 +0900

    Add WkWebView(AutoLayout 적용하기 위해서는 Code로 생성) // RxSwift 적용가능한 형태로 제작(Proxy)

--- AllReview/ViewController/MainViewController.swift
+++ AllReview/ViewController/MainViewController.swift
@@ -22,12 +22,16 @@ class MainViewController: UIViewController, WKUIDelegate {
     private var webRankView: WKWebView!
     private var webMyView: WKWebView!
     
+    private var webViewList: Array<WKWebView>!
+    
     @IBOutlet var headerView: UIView!
     @IBOutlet var bottomView: UIView!
     
     @IBOutlet var webContainer: UIView!
     
+    @IBOutlet var mainViewButton: UIView!
     @IBOutlet var rankViewButton: UIView!
+    @IBOutlet var myViewButton: UIView!
     //    lazy var webView: WKWebView = {
 //        let wv = WKWebView()
 //        wv.uiDelegate = self
@@ -39,24 +43,36 @@ class MainViewController: UIViewController, WKUIDelegate {
         print("viewDidLoad")
         
         let webConfigure = WKWebViewConfiguration()
-        webMainView = WKWebView(frame: .zero, configuration: webConfigure)
-        webRankView = WKWebView(frame: .zero, configuration: webConfigure)
-        webMyView = WKWebView(frame: .zero, configuration: webConfigure)
         
+        let cgRect = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - self.bottomView.bounds.height-self.headerView.bounds.height)
+        webMainView = WKWebView(frame: cgRect, configuration: webConfigure)
+        webRankView = WKWebView(frame: cgRect, configuration: webConfigure)
+        webMyView = WKWebView(frame: cgRect, configuration: webConfigure)
+
+        
+        webViewList = Array<WKWebView>()
+        
+        webViewList.append(webMyView)
+        webViewList.append(webRankView)
+        webViewList.append(webMainView)
+
         self.webMainView.uiDelegate = self
         self.webRankView.uiDelegate = self
         self.webMyView.uiDelegate = self
         
-        let rect = self.webContainer.frame
-        self.webMainView.frame = CGRect(x:0,y:0,width:rect.size.width,height:rect.size.height)
-        self.webRankView.frame = CGRect(x:0,y:0,width:rect.size.width,height:rect.size.height)
-        self.webMyView.frame = CGRect(x:0,y:0,width:rect.size.width,height:rect.size.height)
+        webViewAddWebContainer()
         
-        self.webContainer.addSubview(webMainView)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(rankButtonTapped(_:)))
+        let tap2 = UITapGestureRecognizer(target: self, action: #selector(rankButtonTapped(_:)))
+        let tap3 = UITapGestureRecognizer(target: self, action: #selector(rankButtonTapped(_:)))
+        let tap4 = UITapGestureRecognizer(target: self, action: #selector(rankButtonTapped(_:)))
         
+        mainViewButton.isUserInteractionEnabled = true
+        mainViewButton.addGestureRecognizer(tap)
         rankViewButton.isUserInteractionEnabled = true
-        let tap = UITapGestureRecognizer(target: self, action: #selector(rankButtonTapped))
-        rankViewButton.addGestureRecognizer(tap)
+        rankViewButton.addGestureRecognizer(tap2)
+        myViewButton.isUserInteractionEnabled = true
+        myViewButton.addGestureRecognizer(tap3)
         
         userLoginSession.getLoginData()?.flatMap({ [weak self] data -> Observable<[URLRequest]> in
             let userData = ["memberId":data.data._id]
@@ -73,34 +89,40 @@ class MainViewController: UIViewController, WKUIDelegate {
         }).subscribe(onNext: { [weak self] requests in
             self?.webMainView.load(requests[0])
             self?.webRankView.load(requests[1])
+            self?.webRankView.isHidden = true
             self?.webMyView.load(requests[2])
+            self?.webMyView.isHidden = true
         }).disposed(by: disposeBag)
-    
-//        if #available(iOS 11, *) {
-//          NSLayoutConstraint.activate([
-//          webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//          webView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-//          webView.rightAnchor.constraint(equalTo: view.rightAnchor),
-//          webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
-//
-//        } else {
-//           NSLayoutConstraint.activate([
-//           webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//           webView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 8),
-//           webView.rightAnchor.constraint(equalTo: view.rightAnchor),
-//           webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
-//        }
-    
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        
     }
     
-    @objc func rankButtonTapped() {
-        self.webContainer.subviews[0].removeFromSuperview()
-        self.webContainer.addSubview(self.webRankView)
+    func webViewAddWebContainer() {
+        for item in self.webViewList {
+            self.webContainer.addSubview(item)
+            item.translatesAutoresizingMaskIntoConstraints = true
+            item.leadingAnchor.constraint(equalTo: self.webContainer.leadingAnchor).isActive = true
+            item.trailingAnchor.constraint(equalTo: self.webContainer.trailingAnchor).isActive = true
+            item.topAnchor.constraint(equalTo: self.webContainer.topAnchor).isActive = true
+            item.bottomAnchor.constraint(equalTo: self.webContainer.bottomAnchor).isActive = true
+        }
+    }
+    
+    @objc func rankButtonTapped(_ sender: UITapGestureRecognizer) {
+        self.webMyView.isHidden = true
+        self.webRankView.isHidden = true
+        self.webMainView.isHidden = true
+        
+        if(sender.view == self.mainViewButton) {
+            self.webMainView.isHidden = false
+        } else if (sender.view == self.rankViewButton) {
+            self.webRankView.isHidden = false
+        } else if (sender.view == self.myViewButton) {
+            self.webMyView.isHidden = false
+        }
     }
 }
---
+++ AllReview/ViewModel/MainViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  MainViewModel.swift
+//  AllReview
+//
+//  Created by 정하민 on 2019/10/31.
+//  Copyright © 2019 swift. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class MainViewModel {
+    
+    private let request = OneLineReviewAPI.sharedInstance
+    private let disposeBag = DisposeBag()
+    private let backgroundScheduler = SerialDispatchQueueScheduler(qos: .default)
+    private var urlMaker = OneLineReviewURL()
+    
+    let didSignIn = PublishSubject<Void>()
+    let didFailSignIn = PublishSubject<Error>()
+    
+    func testLoginTapped() {
+        let data = [
+            "memberId": "5d25b1a9b692d8fa466e8a75",
+            "memberEmail": "shjo@naver.com",
+            "platformCode": "EM",
+            "deviceCheckId": "macos-yond",
+            "password": "alfkzmf1!"
+        ]
+
+        self.urlMaker.rxMakeLoginURLComponents(.login, data).flatMap { [weak self] (request) -> Observable<userLoginSession> in
+            return (self?.request.rxTestLogin(userData: data).observeOn(self!.backgroundScheduler))!
+        }.subscribe(onNext: { [weak self] resData in
+            print("Nexst")
+            UserLoginSession.sharedInstance.setLoginData(data: resData)
+            self?.didSignIn.onNext(())
+        }, onError: { [weak self] err in
+            self?.didFailSignIn.onNext(err)
+        }).disposed(by: disposeBag)
+    }
+    
+    func socialRegister() {
+        
+    }
+
+}
