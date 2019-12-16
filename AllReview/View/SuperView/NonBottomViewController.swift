fe50f0e241682e187ebe3960cd1ed8873aff4e22
wjdgkals23 <wjdgkals74@gmail.com>
2019-12-16 13:30:50 +0900

    Super View 요소 셋팅

--- AllReview/View/SuperView/NonBottomViewController.swift
+++ AllReview/View/SuperView/NonBottomViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class NonBottomViewController: UIViewController {
 
+    @IBOutlet var headerView: UIView!
+    @IBOutlet var backButton: UIButton!
+    
+    @IBOutlet var containerView: UIView! // containerChange 함수 필요!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    func onAddChildController(vc: UIViewController) {
+        self.addChild(vc)
+        self.containerView.addSubview(vc.view)
+        vc.didMove(toParent: self)
+    }
 
     /*
     // MARK: - Navigation
