85345d120a8390565cfc63e5a81035fb132f17cc
wjdgkals23 <wjdgkals74@gmail.com>
2020-02-29 11:50:27 +0900

    Setting Test Code For Bot

---
+++ OneLineReview (dev)Tests/OneLineReview__dev_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  OneLineReview__dev_Tests.swift
+//  OneLineReview (dev)Tests
+//
+//  Created by 정하민 on 2020/02/28.
+//  Copyright © 2020 swift. All rights reserved.
+//
+
+import XCTest
+
+class OneLineReview__dev_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        print("Success")
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
